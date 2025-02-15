e8c92d6f58179d252828fe6088315d780d05546b
Elly <145195760+ellypaws@users.noreply.github.com>
2025-02-15 18:00:55 +0800

    refactor: use strings.Builder and include logging

--- bot/handler.go
+++ bot/handler.go
@@ -3,6 +3,7 @@ package bot
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -10,37 +11,42 @@ import (
 func (b *Bot) registerMainHandler() {
 	b.Discord.Session.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
 		if m.Author.Bot {
+			log.Printf("Ignoring message from bot %s", m.Author.Username)
 			return
 		}
 
 		if m.ChannelID != b.Discord.Channel.ID {
+			log.Printf("Ignoring message from channel %s, want %s", m.ChannelID, b.Discord.Channel.ID)
 			return
 		}
 
-		var forwardText string
+		var forwardText strings.Builder
 		if m.Content != "" {
-			forwardText += fmt.Sprintf("**%s**: %s\n", m.Author.Username, m.Content)
+			forwardText.WriteString(fmt.Sprintf("**%s**: %s\n", m.Author.Username, m.Content))
 		}
 
 		for _, embed := range m.Embeds {
 			if embed.Title != "" {
-				forwardText += fmt.Sprintf("\n**%s**", embed.Title)
+				forwardText.WriteString(fmt.Sprintf("\n**%s**", embed.Title))
 			}
 			if embed.Description != "" {
-				forwardText += fmt.Sprintf("\n%s\n", embed.Description)
+				forwardText.WriteString(fmt.Sprintf("\n%s\n", embed.Description))
 			}
 			if embed.Image != nil {
-				forwardText += fmt.Sprintf("\n%s\n", embed.Image.URL)
+				forwardText.WriteString(fmt.Sprintf("\n%s\n", embed.Image.URL))
 			}
 		}
 
-		if forwardText == "" {
+		if forwardText.Len() == 0 {
+			log.Printf("Ignoring message with no content")
 			return
 		}
 
-		err := b.Telegram.Send(forwardText)
+		log.Printf("Forwarding message to Telegram: %s", forwardText.String())
+		err := b.Telegram.Send(forwardText.String())
 		if err != nil {
 			log.Printf("Error forwarding message to Telegram: %v", err)
 		}
+		log.Printf("Message forwarded to Telegram")
 	})
 }
